841682e22bb919ad43f36a6b65e9320fbaf23ed7
Michell Brito <michell@dyn-160-39-247-5.dyn.columbia.edu>
2019-02-09 10:42:47 -0500

    Javascript solution to palindrome question

---
+++ palindrome.js
@@ -0,0 +1,21 @@
+/* Write a
+function that takes in a single word as a string and returns true
+if it’ s a palindrome and false otherwise(a palindrome is spelled the same way forwards and backwards). */
+
+
+let checkPalindrom = function (word) {
+
+    // splits the given word into an array, reverses the word, and then finally joins the word together 
+    reverseWord = word.split("").reverse().join("");
+
+    // checks to see if the user word is equal to the reverse word and if it is console.log true if not console.log false
+    if (word === reverseWord) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+checkPalindrom("noon"); // should console.log true noon = noon
+checkPalindrom("horse"); // should console.log false horse != esroh
+checkPalindrom("racecar"); // should consoe.log true racecar = racecar
