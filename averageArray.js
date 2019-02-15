0516663f0599aee06bbf0b011d15d418409fda94
Michell Brito <michell@dyn-160-39-246-26.dyn.columbia.edu>
2019-02-14 22:05:02 -0500

    fixed errors

--- averageArray.js
+++ averageArray.js
@@ -1,12 +1,12 @@
 // Write a function that takes in an array of numbers
 // and outputs the average of all the numbers.
 
-let averageArray = function (numArray){
+let averageArray = function (numArray) {
     // keeps track of the sum 
     let sum = 0;
-    
+
     // loops through array
-    for (let i = 0; i < numArray.length; i++){
+    for (let i = 0; i < numArray.length; i++) {
         // adds the numbers in the array to sum
         sum = sum + numArray[i];
     }
@@ -14,7 +14,6 @@ let averageArray = function (numArray){
     console.log(sum / numArray.length);
 };
 
-averageArray([1,4,7]); // should console.log 4 
-averageArray([10,5]); // should console.log 7.5
-averageArray([1.5,3,2.5,1]); // should console.log 2
-
+averageArray([1, 4, 7]); // should console.log 4 
+averageArray([10, 5]); // should console.log 7.5
+averageArray([1.5, 3, 2.5, 1]); // should console.log 2
