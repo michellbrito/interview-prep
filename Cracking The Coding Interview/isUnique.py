b5d483a9b7840c96527ce24f9c66b2554396fa2f
Michell Brito <michell@Michells-MBP.grandenetworks.net>
2019-06-18 22:43:28 -0500

    isUnique CH1 1.1

--- Cracking The Coding Interview/isUnique.py
+++ Cracking The Coding Interview/isUnique.py
@@ -1,16 +1,8 @@
-
-
-## take in a string
-
-# turn that string into a array
-
-#loop through that array
-
-#loop through that array again in order to compare the first and second letter
-
-# return true if all letters don't repeate, return false if more than one letter
-#is the same
-
+'''
+THE PROBLEM
+ - Implement an algorithm to determine if a string has all unique characters.
+ What if you cannot use additional data structures?
+'''
 
 
 def isUnique(userString):
@@ -25,3 +17,15 @@ def isUnique(userString):
                 
             
 isUnique("testing")
+
+
+
+'''
+MY LOGIC
+- take in a string
+- turn that string into a array
+- loop through that array
+- loop through that array again in order to compare the first and second letter
+- return true if all letters don't repeate,
+- return false if more than one letter is the same
+'''
