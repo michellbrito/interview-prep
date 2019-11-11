8de40a03eec7f388f3abfe9d587c105990b5acd7
michell brito <michellbp@msn.com>
2019-11-11 17:15:25 -0600

    fresh start, folder restructure

--- reverseString.py
+++
@@ -1,5 +0,0 @@
-def reverseString(s):
-    s = s.reverse()
-    
-
-reverseString()
--- reverseWords.py
+++
@@ -1,7 +0,0 @@
-def reverseWords(s):
-    stringReverse = s.split(" ")
-    stringReverse.reverse()
-    stringReverse = " ".join(stringReverse)[::-1]
-    return stringReverse
-
-reverseWords("Word")
