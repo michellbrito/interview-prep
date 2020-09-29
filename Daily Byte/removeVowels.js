fab005e97237d2b99c8a4d92b75a2143e3dfb4e4
michell brito <michellbp@msn.com>
2020-09-29 08:47:25 -0500

    remove vowels

---
+++ Daily Byte/removeVowels.js
@@ -0,0 +1,18 @@
+/* Given a string s remove all the vowels it contains and return the resulting string.
+Note: In this problem y is not considered a vowel. */
+
+function removeVowels(s) {
+  var noVowels = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] != "a" && s[i] != "e" && s[i] != "i" && s[i] != "o" && s[i] != "u") {
+      noVowels = noVowels + s[i];
+    }
+  }
+
+  return noVowels;
+}
+
+console.log(removeVowels("aeiou")); // -> ""
+console.log(removeVowels("byte")); // -> "byt"
+console.log(removeVowels("xyz")); // -> "xyz"
