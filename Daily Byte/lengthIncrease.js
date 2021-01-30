3f25f3368427bc8bae333c790bbfec10b6825a8e
michell brito <michellbp@msn.com>
2021-01-30 08:32:15 -0500

    length of the longest increasing subarray

--- Daily Byte/lengthIncrease.js
+++ Daily Byte/lengthIncrease.js
@@ -19,4 +19,4 @@ function lengthIncrease(nums) {
 
 console.log(lengthIncrease([1, 2, 3])); // -> 3 because 1, 2, 3 is increasing 
 console.log(lengthIncrease([3, 4, 1, 2, 8])); // -> 3 because 1, 2, 8 is increasing
-console.log(lengthIncrease([3, 4, 8, 7, 0])) // -> 1 because 3, 4 is increasing
+console.log(lengthIncrease([3, 4, 8, 7, 0])); // -> 1 because 3, 4 is increasing
